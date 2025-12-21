1b4ce37154a79a83c44eedbfa308bec31f3297c0
Kurosbani <dedicbenjamin111@gmail.com>
2025-12-21 15:57:01 +0100

    Vollständige python Skript kommentierung

---
+++ docs/lambdascript.md
@@ -0,0 +1,5 @@
+# Lambda Script
+
+Der Lambda Script der verwendet wird ist ein Python Skript.
+
+## Aufbau & Erklärung
--- function.py
+++ function.py
@@ -1,18 +1,26 @@
+#Autor: Benjamin Dedic
+#Datum: 21/12/2025
+
+#Als vorlage wurde der Beispielscode von AWS genommen:
+#https://docs.aws.amazon.com/de_de/rekognition/latest/dg/celebrities-procedure-image.html
+
 import boto3
 import json
 import os
 
 def lambda_handler(event, context):
-    # Extract bucket and photo from S3 trigger event
+    # Extrahiert Bucket- und Foto Name vom S3 trigger event
     bucket = event['Records'][0]['s3']['bucket']['name']
     photo = event['Records'][0]['s3']['object']['key']
     
-    # Output bucket (different from input)
+    # Output bucket (MIT UMGEBUNGSVARIABLEN GESETZT)
     output_bucket = os.environ['OUT_BUCKET']
     
+    # Erstellt Clients für Bilderkennung und Dateispeicherung
     client = boto3.client('rekognition')
     s3 = boto3.client('s3')
     
+    # Ruft die API auf um Celebrities im Bild zu erkennen
     response = client.recognize_celebrities(
         Image={
             'S3Object': {
@@ -22,13 +30,14 @@ def lambda_handler(event, context):
         }
     )
     
-    # Build results object
+    # Erstellt resultst Liste
     results = {
         'source_image': photo,
         'celebrity_count': len(response['CelebrityFaces']),
         'celebrities': []
     }
     
+    # Durchläuft alle erkannten Celebrities und speichert sie in der results Liste
     for celebrity in response['CelebrityFaces']:
         results['celebrities'].append({
             'Name': celebrity['Name'],
@@ -38,10 +47,11 @@ def lambda_handler(event, context):
             'Urls': celebrity['Urls']
         })
     
-    # Create output filename (e.g., image.jpg → image.json)
+    # Erstellt den Namen der JSON aus dem Namen des Bildes 
+    # (image.jpg → image.json)
     output_key = photo.rsplit('.', 1)[0] + '.json'
     
-    # Save to output bucket
+    # Speichert ergebnisse als JSON im output Bucket
     s3.put_object(
         Bucket=output_bucket,
         Key=output_key,
@@ -49,8 +59,10 @@ def lambda_handler(event, context):
         ContentType='application/json'
     )
     
+    # Bestätigung in cloudwatch
     print(f"Results saved to s3://{output_bucket}/{output_key}")
     
+    # Gibt bei erfolg positiven status code und speicherort
     return {
         'statusCode': 200,
         'output_location': f"s3://{output_bucket}/{output_key}"
